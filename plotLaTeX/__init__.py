7d71bf575638711ce841fee7858ec2e6f9df062a
JacobTh98 <jacob.thoenes@uni-rostock.de>
2024-12-18 16:11:49 +0100

    added 3 bar plots

--- plotLaTeX/__init__.py
+++ plotLaTeX/__init__.py
@@ -1,5 +1,6 @@
 from .line_plot import LinePlot
 from .hist_plot import HistPlot
 from .box_plot import BoxPlot
+from .bar_plot import Barplot, MultipleBars
 
-__all__ = ["LaTeXplot", "HistPlot", "BoxPlot"]
+__all__ = ["LaTeXplot", "HistPlot", "BoxPlot", "Barplot", "MultipleBars"]
